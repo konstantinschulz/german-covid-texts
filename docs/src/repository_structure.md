ac0ff710ba6c383e6f455378ded95ea3381c2d9f
Konstantin Schulz <Konstantin.Schulz@dfki.de>
2021-12-07 14:00:22 +0100

    add unit test for internal links in docs

--- docs/src/repository_structure.md
+++ docs/src/repository_structure.md
@@ -1,5 +1,5 @@
 # Uploading New Documents
-Each repository that you use should have its own subfolder in the [raw folder](/data/sources/raw), such as "google-scholar" or "pub-med". 
+Each repository that you use should have its own subfolder in the [raw folder](../../data/sources/raw), such as "google-scholar" or "pub-med". 
 
 Each document must be placed in the subfolder of the repository where it was found. 
 
---
+++ scripts/test_docs.py
@@ -0,0 +1,32 @@
+import glob
+import os.path
+import re
+import unittest
+
+from config import Config
+
+
+class DocsTestCase(unittest.TestCase):
+    def test_links_internal(self):
+        """Checks whether all pointers within the repository are working correctly."""
+        for file_path in glob.glob(Config.docs_src_dir + '/**/*.md', recursive=True):
+            with open(file_path) as f:
+                content: str = f.read()
+                # find all indices where a link follows
+                start_indices: list[int] = [m.start() for m in re.finditer(r']\(', content)]
+                start_indices.append(len(content) - 1)
+                for i in range(len(start_indices) - 1):
+                    opening_bracket_idx: int = start_indices[i] + 1
+                    closing_bracket_idx: int = content.find(")", opening_bracket_idx, start_indices[i + 1])
+                    link: str = content[opening_bracket_idx + 1:closing_bracket_idx]
+                    # do not check external links
+                    if link.startswith("http"):
+                        continue
+                    base_dir: str = os.path.dirname(file_path)
+                    target_path: str = os.path.join(base_dir, link)
+                    # verify that the target path points to a valid, existing file or folder
+                    self.assertTrue(os.path.exists(target_path))
+
+
+if __name__ == '__main__':
+    unittest.main()
