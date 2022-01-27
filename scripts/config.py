b809d16309aedf3178d59e1bf2ca02e7a43035d5
Konstantin Schulz <Konstantin.Schulz@dfki.de>
2022-01-27 14:45:18 +0100

    refactoring

--- scripts/config.py
+++ scripts/config.py
@@ -11,6 +11,7 @@ class Config:
     data_raw_dir: str = os.path.join(sources_dir, "raw")
     docs_dir: str = os.path.join(root_dir, "docs")
     docs_src_dir: str = os.path.join(docs_dir, "src")
+    encoding: str = "utf8"
     sample_raw_dir: str = os.path.join(data_raw_dir, "sample")
     sample_tok_dir: str = os.path.join(tok_dir, "sample")
     sample_txt_dir: str = os.path.join(plain_text_dir, "sample")
--- scripts/tokenize_sources.py
+++ scripts/tokenize_sources.py
@@ -11,52 +11,47 @@ import json
 from config import Config
 
 
-def tokenize():
+def get_text_from_json(file) -> str:
+    """ Extracts plain text from a JSON file. """
+    json_file = os.path.join(Config.sample_raw_dir, file)
+    content: str = ""
+    with open(json_file, 'r', encoding="utf8") as f:
+        data: dict = json.load(f)
+        for section in data["sections"]:
+            content += " " + section["sectionTitle"]
+            content += section["text"]
+    return content
+
+
+def tokenize() -> None:
     """ Splits raw source data into sentences and tokens. """
     for file in tqdm(os.listdir(Config.sample_raw_dir)):
-        if not ".json" in file:
-            raw: dict = parser.from_file(os.path.join(Config.sample_raw_dir, file), service="text", xmlContent=True)
-            # remove line breaks and trailing whitespace
-            content: str = raw['content'].replace("\n", " ").strip()
-            # remove tabs
-            content = re.sub(r"\t", " ", content)
-            # collapse duplicate whitespace into single whitespace
-            content = re.sub(r" +", " ", content)
-            # change file extension to .txt
-            target_file_name: str = os.path.splitext(file)[0] + ".txt"
-            target_path: str = os.path.join(Config.sample_txt_dir, target_file_name)
-            with open(target_path, "w+", encoding="utf8") as f:
-                # write plain text to file
-                f.write(content)
-            language: str = "german"
-            sent_tok: list[list[str]] = []
-            for sent in nltk.sent_tokenize(content, language=language):
-                sent_tok.append(nltk.word_tokenize(sent, language=language))
-            target_path = os.path.join(Config.sample_tok_dir, target_file_name)
-            with open(target_path, "w+", encoding="utf8") as f:
-                # write tokenized text to file
-                f.write("\n".join([" ".join(x) for x in sent_tok]))
+        content: str
+        if ".json" in file:
+            content = get_text_from_json(file)
         else:
-            json_tokenize(file)
-            
-def json_tokenize(file):
-    """ Splits raw source json data into sentences and tokens. """
-    json_file = os.path.join(Config.sample_raw_dir, file)
-    target_file_name: str = os.path.splitext(json_file)[0] + ".txt"
-    target_path: str = os.path.join(Config.sample_txt_dir, target_file_name)
-    with open (json_file, 'r', encoding="utf8") as f:
-        data = json.load(f)
-        content = ""
-        for section in data["sections"]:
-            content = content + "\n" + section["sectionTitle"] 
-            content += section["text"]
+            raw: dict = parser.from_file(os.path.join(Config.sample_raw_dir, file), service="text", xmlContent=True)
+            content = raw['content']
+        # remove line breaks and trailing whitespace
+        content = content.replace("\n", " ").strip()
+        # remove tabs
+        content = re.sub(r"\t", " ", content)
+        # collapse duplicate whitespace into single whitespace
+        content = re.sub(r" +", " ", content)
+        # change file extension to .txt
+        target_file_name: str = os.path.splitext(file)[0] + ".txt"
+        target_path: str = os.path.join(Config.sample_txt_dir, target_file_name)
+        with open(target_path, "w+", encoding=Config.encoding) as f:
+            # write plain text to file
+            f.write(content)
         language: str = "german"
         sent_tok: list[list[str]] = []
         for sent in nltk.sent_tokenize(content, language=language):
             sent_tok.append(nltk.word_tokenize(sent, language=language))
         target_path = os.path.join(Config.sample_tok_dir, target_file_name)
-        with open(target_path, "w+", encoding="utf8") as f:
+        with open(target_path, "w+", encoding=Config.encoding) as f:
             # write tokenized text to file
             f.write("\n".join([" ".join(x) for x in sent_tok]))
+
 
 tokenize()
